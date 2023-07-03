633492bbcb8da9d9e470f40c9ac1b326e1c719ca
xiaoznz <779740337@qq.com>
2023-07-03 15:22:45 +0800

    抄袭成功

--- src/views/SwiperSlide.vue
+++ src/views/SwiperSlide.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="home">
     <swiper
+      ref="homeSwiper"
       :slidesPerView="1"
       :loop="true"
       :centeredSlides="true"
@@ -9,7 +10,8 @@
         crossFade: true
       }"
       :pagination="{
-        clickable: true
+        clickable: true,
+        renderBullet
       }"
       :autoplay="{
         delay: 2500,
@@ -17,6 +19,8 @@
       }"
       :navigation="false"
       :modules="modules"
+      @init="init"
+      @slideChange="slideChange"
       class="mySwiper"
     >
       <swiper-slide
@@ -43,6 +47,9 @@ import "swiper/scss/effect-fade";
 import service_slide1 from "@/assets/img/swiper_pic01.jpg";
 import service_slide2 from "@/assets/img/swiper_pic02.jpg";
 import service_slide3 from "@/assets/img/swiper-pic03.jpg";
+import { ref } from "vue";
+
+const homeSwiper = ref(null);
 
 const modules = [Autoplay, Pagination, Navigation, A11y, EffectFade];
 
@@ -51,74 +58,160 @@ const swiperSlides = [
   { src: service_slide2 },
   { src: service_slide3 }
 ];
+
+const init = function () {
+  setTimeout(function () {
+    $(".swiper-pagination .swiper-pagination-bullet:eq(0)").addClass("active");
+  }, 0);
+};
+
+const slideChange = function (swiper) {
+  $(".swiper-pagination .swiper-pagination-bullet").removeClass("active");
+  $(
+    ".swiper-pagination .swiper-pagination-bullet:eq(" + swiper.realIndex + ")"
+  ).addClass("active");
+};
+
+const renderBullet = function (index, className) {
+  return (
+    '<span class="' +
+    className +
+    '"><b class="num">0' +
+    (index + 1) +
+    '</b><i class="line"></i></span>'
+  );
+};
 </script>
 
 <style lang="scss">
 $color: rgb(43, 121, 237);
 
-.mySwiper {
-  height: 760px;
+#home {
+  .mySwiper {
+    height: 760px;
 
-  .slide_img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    transform: rotateY(180deg);
-  }
-
-  .swiper-slide {
-    position: relative;
-
-    .swiper_title {
-      margin-left: 50px;
-      font-family: douyuFont;
-      position: absolute;
-      z-index: 10;
-      top: 50%;
-      transform: translateY(calc(-50%));
-      left: 0;
-      color: white;
-      font-size: 40px;
-
-      span {
-        color: rgba(245, 7, 7, 1);
-      }
-    }
-
-    &::after {
-      content: "";
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
+    .slide_img {
+      object-fit: cover;
       width: 100%;
       height: 100%;
-      background-color: rgba(235, 236, 237, 0.3);
+      transform: rotateY(180deg);
+    }
+
+    .swiper-slide {
+      position: relative;
+
+      .swiper_title {
+        margin-left: 50px;
+        font-family: douyuFont;
+        position: absolute;
+        z-index: 10;
+        top: 50%;
+        transform: translateY(calc(-50%));
+        left: 0;
+        color: white;
+        font-size: 40px;
+
+        span {
+          color: rgba(245, 7, 7, 1);
+        }
+      }
+
+      &::after {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(235, 236, 237, 0.3);
+      }
     }
   }
-}
 
-.swiper-pagination {
-  bottom: 50px !important;
-}
+  .swiper-pagination {
+    right: 4.323%;
+    text-align: center;
+    width: auto;
+    bottom: 50%;
+    left: auto;
+    -webkit-transform: translate3d(0, 50%, 0);
+    transform: translate3d(0, 50%, 0);
+  }
 
-//修改分页器圆点大小
-.swiper-pagination-bullet {
-  width: 14px;
-  height: 14px;
-  margin: 0 13px !important;
-  background-color: white;
-  opacity: 1;
-}
+  .swiper-pagination .swiper-pagination-bullet {
+    margin: 0 0 3em;
+    width: auto;
+    height: auto;
+    background-color: transparent;
+    display: block;
+    opacity: 1;
+    position: relative;
+    -webkit-transition: padding 0.5s linear;
+    transition: padding 0.5s linear;
+  }
 
-.swiper-pagination-bullet-active {
-  background-color: rgba(252, 18, 18, 1);
-}
+  .swiper-pagination .swiper-pagination-bullet:last-child {
+    margin: 0;
+  }
 
-//修改分页器圆点之间的距离
-.swiper-horizontal > .swiper-pagination-bullets .swiper-pagination-bullet,
-.swiper-pagination-horizontal.swiper-pagination-bullets
-  .swiper-pagination-bullet {
-  margin: 0 8px;
+  .swiper-pagination .swiper-pagination-bullet.active {
+    padding: 0 0 16.25em;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet:last-child.active {
+    padding: 16.25em 0 0;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet .num {
+    font-size: 1.125em;
+    color: #fff;
+    font-family: Arial;
+    font-weight: bold;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet .line {
+    /* content: ""; */
+    width: 2px;
+    height: 0;
+    background-color: rgba(255, 255, 255, 0.3);
+    display: block;
+    -webkit-transition: height 0.5s linear;
+    transition: height 0.5s linear;
+    position: absolute;
+    left: 50%;
+    margin-left: -1px;
+    bottom: 0;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet:last-child .line {
+    bottom: auto;
+    top: 0;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet.active .line {
+    height: 14.25em;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet .line::after {
+    content: "";
+    background-color: #fff;
+    width: 100%;
+    height: 0;
+    display: block;
+    -webkit-transition: height 2.5s linear 0.5s;
+    transition: height 2.5s linear 0.5s;
+    position: absolute;
+    top: 0;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet:last-child .line::after {
+    bottom: 0;
+    top: auto;
+  }
+
+  .swiper-pagination .swiper-pagination-bullet.active .line::after {
+    height: 100%;
+  }
 }
 </style>
