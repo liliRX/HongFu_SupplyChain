7e041ed491fe1a6620179b1d432e16dc2c01b63f
xiaoznz <779740337@qq.com>
2023-07-04 17:11:34 +0800

    完成我们的优势

--- src/views/AdvantageView.vue
+++ src/views/AdvantageView.vue
@@ -6,6 +6,7 @@ import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
 import SecondTitle from "../components/SecondTitle.vue";
+import swiperBg from "@/assets/img/about_num_bg.png";
 
 const modules = [Pagination, Navigation, A11y];
 </script>
@@ -34,9 +35,12 @@ const modules = [Pagination, Navigation, A11y];
         :key="`history_swiper_${index}`"
         v-for="(slide, index) in advantages"
       >
-        <div class="img_container">
-          <p class="title">{{ slide.title }}</p>
-          <p class="description">{{ slide.description }}</p>
+        <div class="adv_container">
+          <img :src="swiperBg" class="adv_img" />
+          <div>
+            <p class="description">{{ slide.description }}</p>
+            <p class="title">{{ slide.title }}</p>
+          </div>
         </div>
       </swiper-slide>
     </swiper>
@@ -45,35 +49,58 @@ const modules = [Pagination, Navigation, A11y];
 
 <style lang="scss">
 @import "@/assets/media_container.scss";
+@import "@/assets/font.scss";
 
 #Advantage {
   margin-top: 100px;
   padding-bottom: 40px;
 
   .mySwiper {
+    margin-top: 40px;
     overflow: visible !important;
   }
 
-  .img_container {
-    height: 340px;
+  .adv_container {
+    height: 320px;
     border-radius: 10px;
-    margin-left: 15px;
-    padding: 15px 20px;
+    padding: 40px;
     box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
     background-color: #fff;
+    overflow: hidden;
+    position: relative;
+
+    > div {
+      height: 100%;
+      position: relative;
+      z-index: 3;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      align-items: end;
+    }
+
+    .adv_img {
+      position: absolute;
+      left: -10%;
+      bottom: -20%;
+      z-index: 0;
+      width: 300px;
+      transform: rotateZ(45deg);
+    }
 
     &:hover {
-      background-color: red;
-      background-image: url("@/assets/img/about_num_bg.png");
-      background-size: 55%;
+      background-color: rgba(252, 18, 18, 0.6);
+
+      .adv_img {
+        opacity: 40%;
+      }
 
       .title {
-        display: none;
+        color: white;
       }
 
       .description {
         color: white;
-        display: initial;
       }
     }
 
@@ -81,8 +108,8 @@ const modules = [Pagination, Navigation, A11y];
     z-index: 1;
     -webkit-transform-origin: 50% 100%;
     transform-origin: 50% 100%;
-    -webkit-transition: all 0.2s ease;
-    transition: all 0.2s ease;
+    -webkit-transition: all 0.4s ease;
+    transition: all 0.4s ease;
     -webkit-transform: scale(1);
     transform: scale(1);
 
@@ -99,17 +126,15 @@ const modules = [Pagination, Navigation, A11y];
     }
 
     .title {
-      display: initial;
-    }
-
-    .description {
-      display: none;
+      font-size: 44px;
+      font-family: PangMen;
+      color: rgba(252, 18, 18, 1);
     }
 
     p {
       font-weight: bold;
-      font-size: 16px;
-      line-height: 28px;
+      font-size: 24px;
+      line-height: 34px;
     }
 
     //> img {
