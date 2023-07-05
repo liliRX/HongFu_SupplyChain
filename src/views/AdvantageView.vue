398d55cd1b9f0c7e0ff001e04423f393b3ef82ab
xiaoznz <779740337@qq.com>
2023-07-05 09:36:16 +0800

    完成我们的优势余下的样式

--- src/views/AdvantageView.vue
+++ src/views/AdvantageView.vue
@@ -5,28 +5,26 @@ import { Navigation, Pagination, A11y } from "swiper";
 import "swiper/scss";
 import "swiper/scss/navigation";
 import "swiper/scss/pagination";
-import SecondTitle from "../components/SecondTitle.vue";
 import swiperBg from "@/assets/img/about_num_bg.png";
+import { ref } from "vue";
 
 const modules = [Pagination, Navigation, A11y];
+const activeLi = ref("战略合作");
 </script>
 
 <template>
   <div id="Advantage">
-    <SecondTitle chinese-title="我们的优势" english-title="Advantage" />
+    <header class="media_container adv_header">我们的优势</header>
     <swiper
       :spaceBetween="40"
       :slides-per-view="2"
       :slides-offset="50"
       :space-between="40"
       :speed="500"
-      :pagination="{
-        // el: '#history .swiper-pagination',
-        clickable: true,
-        renderBullet
-      }"
+      :pagination="{ type: 'progressbar' }"
       :navigation="{
-        clickable: true
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
       }"
       :modules="modules"
       class="mySwiper media_container"
@@ -35,7 +33,12 @@ const modules = [Pagination, Navigation, A11y];
         :key="`history_swiper_${index}`"
         v-for="(slide, index) in advantages"
       >
-        <div class="adv_container">
+        <div
+          :class="`adv_container ${
+            activeLi === slide.title ? 'adv_container_active' : ''
+          }`"
+          @mouseenter="() => (activeLi = slide.title)"
+        >
           <img :src="swiperBg" class="adv_img" />
           <div>
             <p class="description">{{ slide.description }}</p>
@@ -43,6 +46,12 @@ const modules = [Pagination, Navigation, A11y];
           </div>
         </div>
       </swiper-slide>
+      <div class="swiper-button-prev">
+        <i :class="`iconfont icon-jiantou_xiangzuo`"></i>
+      </div>
+      <div class="swiper-button-next">
+        <i :class="`iconfont icon-jiantou_xiangyou`"></i>
+      </div>
     </swiper>
   </div>
 </template>
@@ -55,9 +64,32 @@ const modules = [Pagination, Navigation, A11y];
   margin-top: 100px;
   padding-bottom: 40px;
 
+  .adv_header {
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 40px;
+  }
+
   .mySwiper {
     margin-top: 40px;
+    padding-bottom: 80px;
     overflow: visible !important;
+  }
+
+  .adv_container_active {
+    background-color: rgba(252, 18, 18, 0.6) !important;
+
+    .adv_img {
+      opacity: 40% !important;
+    }
+
+    .title {
+      color: white !important;
+    }
+
+    .description {
+      color: white !important;
+    }
   }
 
   .adv_container {
@@ -68,6 +100,15 @@ const modules = [Pagination, Navigation, A11y];
     background-color: #fff;
     overflow: hidden;
     position: relative;
+    user-select: none;
+    cursor: pointer;
+    z-index: 1;
+    -webkit-transform-origin: 50% 100%;
+    transform-origin: 50% 100%;
+    -webkit-transition: all 0.4s ease;
+    transition: all 0.4s ease;
+    -webkit-transform: scale(1);
+    transform: scale(1);
 
     > div {
       height: 100%;
@@ -87,31 +128,6 @@ const modules = [Pagination, Navigation, A11y];
       width: 300px;
       transform: rotateZ(45deg);
     }
-
-    &:hover {
-      background-color: rgba(252, 18, 18, 0.6);
-
-      .adv_img {
-        opacity: 40%;
-      }
-
-      .title {
-        color: white;
-      }
-
-      .description {
-        color: white;
-      }
-    }
-
-    cursor: pointer;
-    z-index: 1;
-    -webkit-transform-origin: 50% 100%;
-    transform-origin: 50% 100%;
-    -webkit-transition: all 0.4s ease;
-    transition: all 0.4s ease;
-    -webkit-transform: scale(1);
-    transform: scale(1);
 
     header {
       display: flex;
@@ -136,12 +152,49 @@ const modules = [Pagination, Navigation, A11y];
       font-size: 24px;
       line-height: 34px;
     }
+  }
 
-    //> img {
-    //  width: 100%;
-    //  height: 100%;
-    //  border-radius: 10px;
-    //}
+  .swiper-pagination-progressbar {
+    bottom: 20px;
+    left: initial;
+    right: 0;
+    top: initial;
+    width: 86%;
+    background-color: rgba(239, 123, 123, 0.4);
+  }
+
+  .swiper-pagination-progressbar-fill {
+    background-color: red;
+  }
+
+  .swiper-button-prev {
+    left: 0;
+    bottom: 0;
+    top: initial;
+    color: grey;
+
+    i {
+      font-size: 30px;
+    }
+
+    &::after {
+      display: none;
+    }
+  }
+
+  .swiper-button-next {
+    left: 50px;
+    bottom: 0;
+    top: initial;
+    color: grey;
+
+    i {
+      font-size: 30px;
+    }
+
+    &::after {
+      display: none;
+    }
   }
 }
 </style>
