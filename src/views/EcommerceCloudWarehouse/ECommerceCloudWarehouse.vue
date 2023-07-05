1649062185da8b72391c9a839eff2d0ebed4b573
xiaoznz <779740337@qq.com>
2023-07-05 15:21:01 +0800

    解决问题

--- src/views/EcommerceCloudWarehouse/ECommerceCloudWarehouse.vue
+++ src/views/EcommerceCloudWarehouse/ECommerceCloudWarehouse.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="header">
-    <img class="img" src="@/assets/img/ecommerce-bg.jpg" alt="" />
-    <div class="img_text">
-      <p class="p1">电商云仓</p>
-      <p class="p2">一站式电商仓配</p>
+  <div id="ECommerceCloudWarehouse">
+    <div class="header">
+      <img class="img" src="@/assets/img/ecommerce-bg.jpg" alt="" />
+      <div class="img_text">
+        <p class="p1">电商云仓</p>
+        <p class="p2">一站式电商仓配</p>
+      </div>
     </div>
-  </div>
-  <div class="intro">
-    <div class="intro_img">
-      <p class="subtitle">仓配一体化服务</p>
-      <img src="@/assets/img/process.png" alt="仓配流程图" />
+    <div class="intro">
+      <div class="intro_img">
+        <p class="subtitle">仓配一体化服务</p>
+        <img src="@/assets/img/process.png" alt="仓配流程图" />
+      </div>
       <div class="grid-container">
         <div class="left">
           <div class="image-container">
@@ -20,16 +22,17 @@
         <div class="right">
           <div v-for="image in images" :key="image.id" class="image-item">
             <div class="image-container">
-              <img :src="image.src" :alt="image.alt" />
+              <img class="right_img" :src="image.src" />
               <div class="title">{{ image.title }}</div>
-              <div class="overlay"></div>
+              <div class="overlay" />
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="intro_video">
-      <p class="subtitle">仓储视频位置</p>
+      <div class="intro_video">
+        <p class="subtitle">仓储视频位置</p>
+        123131
+      </div>
     </div>
   </div>
 </template>
@@ -39,123 +42,141 @@ import { images } from "@/utils/common.js";
 </script>
 
 <style lang="scss" scoped>
-.header {
-  height: 760px;
-  position: relative;
+@import "@/assets/media_container.scss";
 
-  .img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
+#ECommerceCloudWarehouse {
+  .header {
+    height: 760px;
+    position: relative;
 
-  > .img_text {
-    position: absolute;
-    z-index: 10;
-    top: 50%;
-    transform: translateY(-50%);
-    left: 13%;
-    color: white;
-
-    > .p1 {
-      font-size: 45px;
-      font-weight: 400;
-      margin-bottom: 10px;
+    .img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
     }
 
-    > .p2 {
-      font-size: 24px;
-      font-weight: 400;
-      letter-spacing: 10px;
+    > .img_text {
+      position: absolute;
+      z-index: 10;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 13%;
+      color: white;
+
+      > .p1 {
+        font-size: 45px;
+        font-weight: 400;
+        margin-bottom: 10px;
+      }
+
+      > .p2 {
+        font-size: 24px;
+        font-weight: 400;
+        letter-spacing: 10px;
+      }
+    }
+
+    &::after {
+      content: "";
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(20, 20, 20, 0.3);
     }
   }
-
-  &::after {
-    content: "";
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(20, 20, 20, 0.3);
-  }
-}
-
-.intro {
-  margin-top: 50px;
-  border: red 3px solid;
 
   .subtitle {
     text-align: center;
-    font-size: 20px;
+    font-size: 24px;
+    font-weight: bold;
     letter-spacing: 3px;
   }
 
-  .intro_img {
+  .intro {
+    width: 1320px;
+    margin: 50px auto 0;
+
+    .intro_img {
+      > img {
+        width: 100%;
+        height: 430px;
+      }
+    }
+
     .grid-container {
+      height: 400px;
       display: grid;
       grid-template-columns: 1fr 1fr;
       grid-gap: 20px;
-    }
 
-    .left {
-      display: flex;
-      justify-content: center;
-      align-items: center;
+      .overlay {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(
+          to top,
+          rgba(0, 0, 0, 0.8) 0%,
+          rgba(0, 0, 0, 0) 100%
+        );
+      }
 
-      .image-container {
-        position: relative;
+      .left {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
 
-        > img {
-          height: 450px;
-          width: 100%;
+        .image-container {
+          position: relative;
+
+          img {
+            width: 100%;
+            height: 100%;
+          }
+        }
+      }
+
+      .right {
+        width: 100%;
+        height: 100%;
+        display: grid;
+        grid-template-areas:
+          "top-right top-left"
+          "bottom-right bottom-left";
+        grid-gap: 20px;
+
+        .image-item {
+          overflow: hidden;
+          position: relative;
+
+          .right_img {
+            width: 100%;
+            height: 100%;
+          }
         }
       }
     }
 
-    .right {
-      display: grid;
-      grid-template-areas:
-        "top-right top-left"
-        "bottom-right bottom-left";
-      grid-gap: 20px;
-
-      .image-item {
-        position: relative;
-
-        img {
-          width: 100%;
-          height: 200px;
-        }
-      }
+    .title {
+      position: absolute;
+      left: 10px;
+      bottom: 10px;
+      background-color: rgba(0, 0, 0, 0.7);
+      color: #fff;
+      padding: 5px;
     }
   }
 
-  .title {
-    position: absolute;
-    left: 10px;
-    bottom: 10px;
-    background-color: rgba(0, 0, 0, 0.7);
-    color: #fff;
-    padding: 5px;
+  .intro_video {
+    margin-top: 80px;
+    margin-bottom: 20px;
   }
-
-  .overlay {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(
-      to top,
-      rgba(0, 0, 0, 0.8) 0%,
-      rgba(0, 0, 0, 0) 100%
-    );
-  }
-}
-
-.intro_video {
-  margin-top: 80px;
 }
 </style>
